78b2d3089bf3e942da1d1581fa68fd9a32ed02d1
mainlyetcetera <ericcampbell294@gmail.com>
2021-03-19 15:14:22 -0600

    Add mapToDispatch to Form

--- src/components/AddTodoForm.js
+++ src/components/AddTodoForm.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react'
 import { addToDo } from '../actions'
+import { connect } from  'react-redux'
 
 class AddTodoForm extends Component {
   constructor(props) {
@@ -32,4 +33,8 @@ class AddTodoForm extends Component {
   }
 }
 
-export default AddTodoForm;
+const mapDispatchToProps = dispatch => ({
+  addToDo: text => dispatch( addToDo(text) )
+})
+
+export default connect(null, mapDispatchToProps)(AddTodoForm)
